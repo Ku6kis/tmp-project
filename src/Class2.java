3001d3b1f8dc586c655c71d6c6e41a8200ba05ca
Kasperi Kukkamaki <kasperi.kukkamaki@tuni.fi>
2025-04-22 16:05:06 +0300

    feat(Class2): Add a method handling letter differences
        Added isOneLetterAway method in boolean to check that each word differ exactly by one letter to Class2
    
        Refs: #4

--- src/Class2.java
+++ src/Class2.java
@@ -1,14 +1,14 @@
-import java.io.Console;  
+import java.io.Console;
 
 public class MyApp {
     public static void main(String [] args) {
-        Console c = System.console(); 
+        Console c = System.console();
 
         String OriginalWord = c.readLine("give a word, any word:D!  : ");
 
 
 
-        
+
             while (true) {
             String WordAfter = c.readLine("Give a word that differs in only one letter: ");
 
@@ -20,9 +20,25 @@ public class MyApp {
                 break;
             }
         }
-        
+
         }
     }
+
+    public static boolean isOneLetterAway(String a, String b) {
+        if (a.length() != b.length()) {
+            return false;
+        }
+
+        int DIFFERENCES = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                DIFFERENCES++;
+            }
+        }
+
+        return DIFFERENCES == 1;
+    }
+}
 
 
 
