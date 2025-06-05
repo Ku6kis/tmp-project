7f34729f026baf4bcbd9c8b57303aaee08e3d089
Kasperi Kukkamaki <kukkamakikasperi@gmail.com>
2025-06-05 22:15:08 +0300

    refactor(Class1): remove unnecessary console class

--- src/Class1.java
+++ src/Class1.java
@@ -2,7 +2,6 @@ import java.io.Console;
 
 public class MyApp {
     public static void main(String [] args) {
-        Console c = System.console(); 
         int luku = 1;
         while (luku < 11){
         System.out.println(luku);
