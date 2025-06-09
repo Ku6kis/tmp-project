aab5c34dfae0cba4068f744faacd90c90aa2c44a
Kasperi Kukkamaki <kukkamakikasperi@gmail.com>
2025-06-09 23:36:11 +0300

    feat(Class3): Make class MyApp
    Made MyApp to ask user two numbers and only print bigger of those
    
    Refs: #11

--- src/Class3.java
+++ src/Class3.java
@@ -1,3 +1,19 @@
 // Make great code here
 
 
+import java.io.Console;  
+
+public class MyApp {
+    public static void main(String [] args) {
+        Console c = System.console(); 
+
+        System.out.println("Anna luku");
+        int luku = Integer.parseInt(c.readLine());
+        System.out.println("Anna luku");
+        int pasi = Integer.parseInt(c.readLine());
+        if (luku>pasi)
+         System.out.println(luku);
+         if (luku<pasi)
+         System.out.println(pasi);
+    }
+}
